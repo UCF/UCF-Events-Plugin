d0c7cbadece3039b4c528fec53aae53278ae655d
Jim Barnes <jimbarnesdeveloper@gmail.com>
2017-02-20 10:40:33 -0500

    Bumped version.

--- ucf-events.php
+++ ucf-events.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: UCF Events
 Description: Contains shortcode and widget for displaying UCF Events Feeds
-Version: 1.0.1
+Version: 1.0.2
 Author: UCF Web Communications
 License: GPL3
 */
